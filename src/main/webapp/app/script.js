843f6872fe4d6df5c85eae53bef6405612a433d5
Sergiy Illichevskyy <illichso@gmail.com>
2015-10-22 22:34:51 +0200

    add form submittion

--- src/main/webapp/app/script.js
+++ src/main/webapp/app/script.js
@@ -2,4 +2,8 @@ var module = angular.module("main",['angularBetterPlaceholder']);
 
 module.controller("formCtrl", function($scope){
     $scope.data = {};
+    $scope.disabled= true;
+    $scope.submit = function(){
+       alert('Form Submitted!!\nEmail: ' + $scope.data.email + '\nPassword: ' + $scope.data.password);
+    };
 });
