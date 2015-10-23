dbb9d68c93c257152c73d8717fb1f99506b6bee9
Sergiy Illichevskyy <illichso@gmail.com>
2015-10-23 22:35:56 +0200

    add http module

--- src/main/webapp/app/script.js
+++ src/main/webapp/app/script.js
@@ -1,9 +1,35 @@
-var module = angular.module("main",['angularBetterPlaceholder']);
+var module = angular.module('demoApp', ['ngMockE2E']).config(function($provide){
+    $provide.decorator('$httpBacked', function($delegate){
+        var proxy = function(method, url, data, callback, header){
+            var intercaptor = function(){
+                var _this = this;
+                _arguments = arguments;
+                setTimeout(function(){
+                    callbacl.apply(_this, _arguments);
+                }, 700);
+            };
+            return $delegate.call(this, method, url, data, intercaptor, headers);
+        };
+        for(var key in $delegate){
+            proxy[key] = $delefgate[key];
+        }
+        return proxy;
+    });
+}).run(function($httpBacked){
+    $httpBacked.whenPOST('/login').respond(function(method, url, data){
+        var details = angular.fromJson(data);
+        if(details.email && details.email === 'test@test.com' && details.password && details.password === "test")
+            return [200, {loggedIn: true, userid: 'testid'}, {}];
+        else return[200, {loggedIn: false}, {}];
+    });
+});
+
+module = angular.module("MainCtrl",['angularBetterPlaceholder']);
 
 module.controller("formCtrl", function($scope){
     $scope.data = {};
-    $scope.disabled= true;
+
     $scope.submit = function(){
-       alert('Form Submitted!!\nEmail: ' + $scope.data.email + '\nPassword: ' + $scope.data.password);
+        alert('Form Submitted!!\nEmail: ' + $scope.data.email + '\nPassword: ' + $scope.data.password);
     };
 });
