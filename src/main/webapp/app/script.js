2ea36a85e87e1269b35357707e6e128fa4eabca6
Sergiy Illichevskyy <illichso@gmail.com>
2015-10-27 23:00:38 +0100

    update verification

--- src/main/webapp/app/script.js
+++ src/main/webapp/app/script.js
@@ -19,20 +19,31 @@
     }).run(function($httpBackend){
         $httpBackend.whenPOST('/login').respond(function(method, url, data){
             var details = angular.fromJson(data);
-            if(details.email && details.email === 'test@test.com' && details.password && details.password === "test")
+            if(details.email && details.email === 'test@test.com' && details.password && details.password === "test"){
                 return [200, {loggedIn: true, userid: 'testid'}, {}];
-            else return [200, {loggedIn: false}, {}];
+
+            }else{
+                return [200, {loggedIn: false}, {}];
+            }
         });
     });
 
-    module.controller("MainCtrl", ['$scope', '$http', function($scope, $http){
+    module.controller("MainCtrl", ['$scope', '$http', function($scope, $http) {
         $scope.data = {};
         $scope.loading = false;
+        $scope.postResult = 0;
 
-        $scope.submit = function(){
+        $scope.submit = function () {
             $scope.loading = true;
-            $http.post('/login', $scope.data).success(function(data){console.log('Form success', data)});
-            $scope.loading = false;
+            $http.post('/login', $scope.data).success(function (data) {
+                console.log('Form success', data);
+                if (data.loggedIn) {
+                    $scope.postResult = 1;
+                } else {
+                    $scope.postResult = 2;
+                }
+                $scope.loading = false;
+            });
         };
-    }])
+    }]);
 })();
