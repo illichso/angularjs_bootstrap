5528bf57a3256d2059c88036c5cbc86b49ae9b8e
Sergiy Illichevskyy <illichso@gmail.com>
2015-10-29 23:22:03 +0100

    add routing

---
+++ src/main/webapp/app/readme.txt
@@ -0,0 +1,8 @@
+install nodejs server
+npm install -g http-server
+
+then cd into dir containing your index.html and issue :
+http-server -c-1
+
+which spins up a nodejs httpd which does a dir listing of your static files visible from :
+http://localhost:8080
--- src/main/webapp/app/script.js
+++ src/main/webapp/app/script.js
@@ -1,34 +1,62 @@
 (function(){
-    var module = angular.module('demoApp', ['angularBetterPlaceholder', 'ngMockE2E']).config(function($provide){
-        $provide.decorator('$httpBackend', function($delegate){
-            var proxy = function(method, url, data, callback, headers){
-                var interceptor = function(){
-                    var _this = this,
-                        _arguments = arguments;
-                    setTimeout(function(){
-                        callback.apply(_this, _arguments);
-                    }, 700);
+    var module = angular.module('demoApp', ['angularBetterPlaceholder', 'ngMockE2E', 'ngRoute'])
+        .config(['$provide', '$routeProvider', '$locationProvider', function($provide, $routeProvider, $locationProvider){
+            $provide.decorator('$httpBackend', function($delegate){
+                var proxy = function(method, url, data, callback, headers){
+                    var interceptor = function(){
+                        var _this = this,
+                            _arguments = arguments;
+                        setTimeout(function(){
+                            callback.apply(_this, _arguments);
+                        }, 700);
+                    };
+                    return $delegate.call(this, method, url, data, interceptor, headers);
                 };
-                return $delegate.call(this, method, url, data, interceptor, headers);
-            };
-            for(var key in $delegate){
-                proxy[key] = $delegate[key];
-            }
-            return proxy;
-        });
-    }).run(function($httpBackend){
-        $httpBackend.whenPOST('/login').respond(function(method, url, data){
-            var details = angular.fromJson(data);
-            if(details.email && details.email === 'test@test.com' && details.password && details.password === "test"){
-                return [200, {loggedIn: true, userid: 'testid'}, {}];
+                for(var key in $delegate){
+                    proxy[key] = $delegate[key];
+                }
+                return proxy;
+            });
+            $routeProvider
+                .when('/login', {
+                    templateUrl: 'user.tpl.html',
+                    controller: 'UserCtrl'
+                })
+                .otherwise({redirectTo: '/login'});
+            $locationProvider.html5Mode([true]);
+        }]).run(function($httpBackend){
+            $httpBackend.whenPOST('/login').respond(function(method, url, data){
+                var details = angular.fromJson(data);
+                if(details.email && details.email === 'test@test.com' && details.password && details.password === "test"){
+                    return [200, {loggedIn: true, userid: 'testid'}, {}];
 
-            }else{
-                return [200, {loggedIn: false}, {}];
-            }
+                }else{
+                    return [200, {loggedIn: false}, {}];
+                }
+            });
+            $httpBackend.whenGET(/.*/i).passThrough();
         });
-    });
 
     module.controller("MainCtrl", ['$scope', '$http', function($scope, $http) {
+        $scope.data = {};
+        $scope.loading = false;
+        $scope.postResult = 0;
+
+        $scope.submit = function () {
+            $scope.loading = true;
+            $http.post('/login', $scope.data).success(function (data) {
+                console.log('Form success', data);
+                if (data.loggedIn) {
+                    $scope.postResult = 1;
+                } else {
+                    $scope.postResult = 2;
+                }
+                $scope.loading = false;
+            });
+        };
+    }]);
+
+    module.controller("UserCtrl", ['$scope', '$http', function($scope, $http) {
         $scope.data = {};
         $scope.loading = false;
         $scope.postResult = 0;
